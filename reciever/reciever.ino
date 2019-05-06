3f3edfe65067899d7de5548cef7a3be30ad151c8
ConNoa <constantin.noack@uni-weimar.de>
2019-05-06 14:27:09 +0200

    first try

--- reciever/reciever.ino
+++ reciever/reciever.ino
@@ -1,80 +1,50 @@
-
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
 
 /*
-  Created by Yvan / https://Brainy-Bits.com
-
-  This code is in the public domain...
-  You can: copy it, use it, modify it, share it or just plain ignore it!
-  Thx!
-
+* Arduino Wireless Communication Tutorial
+*       Example 1 - Receiver Code
+*
+* by Dejan Nedelkovski, www.HowToMechatronics.com
+*
+* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
 */
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#define PIN 3
+#define NUM_LEDS 228
+#define BRIGHTNESS 100
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRBW + NEO_KHZ800);
 
 
-// NRF24L01 Module Tutorial - Code for Receiver using Arduino UNO
-
-//Include needed Libraries at beginning
-#include "nRF24L01.h" // NRF24L01 library created by TMRh20 https://github.com/TMRh20/RF24
-#include "RF24.h"
-#include "SPI.h"
-#include "FastLED.h"
-//#include "FastLED_RGBW.h" // FastLED library for WS2812 RGB Stick http://fastled.io/
-
-#define NUM_LEDS 4 // Number of leds on stick
-#define LED_PIN 3 // Digital In (DI) of RGB Stick connected to pin 8 of the UNO
-
-CRGB leds[NUM_LEDS]; // FastLED Library Init
-
-int ReceivedMessage[1] = {000}; // Used to store value received by the NRF24L01
-
-RF24 radio(9, 10); // NRF24L01 used SPI pins + Pin 9 and 10 on the UNO
-
-const uint64_t pipe = 0xE6E6E6E6E6E6; // Needs to be the same for communicating between 2 NRF24L01
+RF24 radio(7, 8); // CE, CSN
+const byte address[6] = "00001";
+const char but1_text[32] = "but_1_pressed";
 
 
-void setup(void) {
+void setup() {
 
-  FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS); // Setup FastLED Library
-  FastLED.clear(); // Clear the RGB Stick LEDs
+  Serial.begin(9600);
+  Serial.print("Setup started");
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+  Serial.println("Setup finished");
 
-  // Light up starting LED's
-  for (int x = 0; x != NUM_LEDS; x++) {
-    leds[x] = CRGB::Red;
-  }
 
-  FastLED.setBrightness(100);
-  FastLED.show();
-
-  radio.begin(); // Start the NRF24L01
-
-  radio.openReadingPipe(1, pipe); // Get NRF24L01 ready to receive
-
-  radio.startListening(); // Listen to see if information received
-
-  pinMode(LED_PIN, OUTPUT); // Set RGB Stick UNO pin to an OUTPUT
 }
-
-void loop(void) {
-
-  while (radio.available())
-  {
-    radio.read(ReceivedMessage, 1); // Read information from the NRF24L01
-
-    if (ReceivedMessage[0] == 111) // Indicates switch is pressed
-    {
-      for (int x = 0; x != NUM_LEDS; x++)
-      {
-        leds[x] = CRGB::Red;
-        FastLED.show();
+void loop() {
+  if (radio.available()) {
+    char text[32] = "";
+    radio.read(&text, sizeof(text));
+      if(but1_text ==  text){
+        Serial.println("Action follows now");
       }
-    }
-    else
-    {
-      for (int x = 0; x != NUM_LEDS; x++)
-      {
-        leds[x] = CRGB::Red;
-        FastLED.show();
-      }
-    }
-    delay(10);
+      Serial.println(text);
   }
 }
